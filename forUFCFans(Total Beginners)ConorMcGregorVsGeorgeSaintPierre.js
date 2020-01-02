db7bbb346e95e30096eee7757673d5019e7bd1d7
thiago1186 <thiago.1186@gmail.com>
2020-01-02 07:27:05 -0300

    Create forUFCFans(Total Beginners)ConorMcGregorVsGeorgeSaintPierre.js

---
+++ forUFCFans(Total Beginners)ConorMcGregorVsGeorgeSaintPierre.js
@@ -0,0 +1,7 @@
+var quote = function(fighter) {
+  if(fighter.toLowerCase() == "george saint pierre"){
+    return "I am not impressed by your performance.";
+  }
+  
+  return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
