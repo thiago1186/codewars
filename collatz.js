a428ffe2eeeffc737445398bbcfc6e0227802437
thiago1186 <thiago.1186@gmail.com>
2020-01-08 08:30:02 -0300

    Create collatz.js

---
+++ collatz.js
@@ -0,0 +1,15 @@
+function collatz(n){
+  let result = ""+n;
+
+  while(n > 1){
+   if(n%2 == 0){
+     n = n/2;
+   }else{
+     n = 3*n + 1;
+   }
+
+   result += "->" + n;
+  }
+
+  return result;
+}
